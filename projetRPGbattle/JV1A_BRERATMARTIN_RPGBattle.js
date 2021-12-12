57d8b7d47b1363eb3f4f6093c741184718912fae
MartinBrerat <93645326+MartinBrerat@users.noreply.github.com>
2021-12-12 14:58:13 +0100

    Add files via upload

---
+++ projetRPGbattle/JV1A_BRERATMARTIN_RPGBattle.js
@@ -0,0 +1,84 @@
+
+var Hp_Golem = document.getElementById("Hp_Golem");
+var Mp_Golem = document.getElementById("Mp_Golem");
+
+Hp_Golem.value = 200;
+Hp_Golem.innerHTML = Hp_Golem.value;
+
+Mp_Golem.value = 50;
+Mp_Golem.innerHTML = Mp_Golem.value;
+
+var atq_Golem = 15;
+
+
+
+var Hp_Demon = document.getElementById("Hp_Demon");
+var Mp_Demon = document.getElementById("Mp_Demon");
+
+Hp_Demon.value = 100;
+Hp_Demon.innerHTML = Hp_Demon.value;
+
+Mp_Demon.value = 100;
+Mp_Demon.innerHTML = Mp_Demon.value;
+
+var atq_Demon = 15;
+
+
+
+var Hp_Guerriere = document.getElementById("Hp_Guerriere");
+var Mp_Guerriere = document.getElementById("Mp_Guerriere");
+
+Hp_Guerriere.value = 150;
+Hp_Guerriere.innerHTML = Hp_Guerriere.value;
+
+Mp_Guerriere.value = 50;
+Mp_Guerriere.innerHTML = Mp_Guerriere.value;
+
+var atq_Guerriere = 15;
+
+
+
+var Hp_Sorciere = document.getElementById("Hp_Sorciere");
+var Mp_Sorciere = document.getElementById("Mp_Sorciere");
+
+Hp_Sorciere.value = 50;
+Hp_Sorciere.innerHTML = Hp_Sorciere.value;
+
+Mp_Sorciere.value = 200;
+Mp_Sorciere.innerHTML = Mp_Sorciere.value;
+
+var atq_Sorciere = 15;
+
+
+
+var attaque = document.getElementById("attaque");
+var défense = document.getElementById("défense");
+var magie = document.getElementById("magie");
+var item = document.getElementById("item");
+
+
+
+var Hp_Corbac1 = document.getElementById("Hp_Corbac1");
+
+Hp_Corbac1.value = 50;
+Hp_Corbac1.innerHTML = Hp_Corbac1.value;
+
+var atq_Corbac1 = 5;
+
+
+
+var Hp_Corbac2 = document.getElementById("Hp_Corbac2");
+
+Hp_Corbac2.value = 50;
+Hp_Corbac2.innerHTML = Hp_Corbac2.value;
+
+var atq_Corbac2 = 5;
+
+
+
+var Hp_boarotaur = document.getElementById("Hp_boarotaur");
+
+Hp_boarotaur.value = 50;
+Hp_boarotaur.innerHTML = Hp_boarotaur.value;
+
+var atq_boarotaur = 5;
